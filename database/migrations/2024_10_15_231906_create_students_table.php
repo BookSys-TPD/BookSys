6e307e076c9250c8a00e06db9c04aaf91129718b
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-10-15 20:21:14 -0300

    chore: inicializando projeto Laravel com models Student, Book e Order

---
+++ database/migrations/2024_10_15_231906_create_students_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStudentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('students', function (Blueprint $table) {
+            $table->id();  // Chave primária
+            $table->string('name');  // Nome do aluno
+            $table->string('registration')->unique();  // Matrícula única
+            $table->timestamps();  // Campos created_at e updated_at
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('students');
+    }
+}
