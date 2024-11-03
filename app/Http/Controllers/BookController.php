946ebee571b43316998f0f85aad16c33b86c93ee
Kauã F. N. Rodrigues <kauarodrigues.fn@gmail.com>
2024-11-03 13:56:20 -0300

    feat(BookController): adiciona rotas para cadastro de livros

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -11,4 +11,26 @@ class BookController extends Controller
     $books = Book::all(); // Busca todos os livros
     return view('books.index', compact('books'));
   }
+  
+  public function store(Request $request) {
+    // Validação dos dados
+    $request->validate([
+        'isbn' => 'required|unique:books|max:255',
+        'titulo' => 'required|max:255',
+        'autor' => 'required|max:255',
+        'categoria' => 'required|max:255',
+    ]);
+
+    // Criação do livro
+    Book::create([
+        'isbn' => $request->isbn,
+        'titulo' => $request->titulo,
+        'autor' => $request->autor,
+        'categoria' => $request->categoria,
+    ]);
+
+    // Redireciona para uma página de sucesso ou lista de livros
+    return redirect()->route('books.create')->with('success', 'Livro cadastrado com sucesso!');
+  }
+
 }
--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
 
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
+
+Route::view('/books/create', 'books.create')->name('books.create');
+Route::post('/books', [BookController::class, 'store'])->name('books.store');
