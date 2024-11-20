9efd0f48994fb6645b1b5ef34420111132c64b7a
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-20 13:42:45 -0300

    refactor: mover rota principal para o topo do arquivo de rotas

--- routes/web.php
+++ routes/web.php
@@ -6,14 +6,14 @@ use App\Http\Controllers\BookController;
 use App\Http\Controllers\StudentController;
 use App\Http\Controllers\OrderController;
 
+Route::get('/', function () {
+    return view('index'); 
+})->name('index');
+
 //BOOKS
 Route::view('/books/create', 'books.create')->name('books.create');
 Route::post('/books', [BookController::class, 'store'])->name('books.store');
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
-
-Route::get('/', function () {
-    return view('index'); 
-})->name('index');
 
 //Students
 Route::get('/students/create', [StudentController::class, 'create'])->name('students.create');
