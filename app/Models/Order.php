9717e950e36057bfd446dc8cea4ecbcaf1e7534c
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-23 08:12:19 -0300

    chore(models): ajustar models para incluir campos estudante_id, book_id, data_devolucao_prevista

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -2,33 +2,12 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Order extends Model
 {
     use HasFactory;
 
-    protected $fillable = [
-        'student_id',
-        'user_id',
-        'book_id',
-        'data_emprestimo',
-        'data_devolucao'
-    ];
-
-    // Relação com o modelo Student
-    public function student() {
-        return $this->belongsTo(Student::class);
-    }
-
-    // Relação com o modelo User
-    public function user() {
-        return $this->belongsTo(User::class);
-    }
-
-    // Relação com o modelo Book
-    public function book() {
-        return $this->belongsTo(Book::class);
-    }
+    protected $fillable = ['student_id', 'book_id', 'data_devolucao_prevista'];
 }
