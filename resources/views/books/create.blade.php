77350247183872662d7ce35dc023bec86bcb8076
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 10:35:28 -0300

    feat(view): adicionar nova view cadastro_livros para gerenciamento de livros

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,49 +1,41 @@
-<!DOCTYPE html>
-<html lang="pt-BR">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <title>Cadastro de Livros</title>
-    <style>
-        body {
-            display: flex;
-            justify-content: center;
-            align-items: center;
-            height: 100vh; /* Ocupa toda a altura da tela */
-            background-color: #f8f9fa; /* Cor de fundo clara */
-        }
-    </style>
-</head>
-<body>
-  <div class="container vh-100 d-flex justify-content-center align-items-center">
-    <div class="card" style="width: 30rem;">
-        <div class="card-body">
-            <h5 class="card-title">Cadastrar Livro</h5>
-            <form action="{{ route('books.store') }}" method="POST">
-                @csrf
-                <div class="mb-3">
-                    <label for="isbn" class="form-label">ISBN</label>
-                    <input type="text" class="form-control" id="isbn" name="isbn" required>
-                </div>
-                <div class="mb-3">
-                    <label for="titulo" class="form-label">Título</label>
-                    <input type="text" class="form-control" id="titulo" name="titulo" required>
-                </div>
-                <div class="mb-3">
-                    <label for="autor" class="form-label">Autor</label>
-                    <input type="text" class="form-control" id="autor" name="autor" required>
-                </div>
-                <div class="mb-3">
-                    <label for="categoria" class="form-label">Categoria</label>
-                    <input type="text" class="form-control" id="categoria" name="categoria" required>
-                </div>
-                <button type="submit" class="btn btn-primary">Cadastrar</button>
-            </form>
-        </div>
-    </div>
-  </div>
+@extends('layouts.app')
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-    </body>
-</html>
+@section('title', 'Cadastro de Empréstimos')
+
+@section('content')
+<form action="{{ route('orders.store') }}" method="POST">
+  @csrf
+  <legend>Cadastro de Livros</legend>
+
+  <fieldset>
+    <div class="form-group">
+      <div class="linha">
+        <div class="campo col-8">
+          <label for="titulo">Título</label>
+          <input type="text" id="titulo" name="titulo">
+        </div>
+        <div class="campo col-4">
+          <label for="isbn">ISBN</label>
+          <input type="text" id="isbn" name="isbn">
+        </div>
+      </div>
+
+      <div class="linha">
+        <div class="campo col-12">
+          <label for="autor">Autor</label>
+          <input type="text" id="autor" name="autor">
+        </div>
+      </div>
+
+      <div class="linha">
+        <div class="campo col-12">
+          <label for="categoria">Categoria</label>
+          <input type="text" id="categoria" name="categoria">
+        </div>
+      </div>
+    </div>
+
+    <input type="submit" class="mt-5" value="Salvar">
+  </fieldset>
+</form>
+@endsection
