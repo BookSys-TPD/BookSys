d1369628cf05f497f4b7202147ea1f4a92a00b07
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 14:46:54 -0300

    Refactor: unificação do estilo e estrutura do formulário de cadastro de livros com o de edição

--- resources/views/books/create.blade.php
+++ resources/views/books/create.blade.php
@@ -1,41 +1,41 @@
 @extends('layouts.app')
 
-@section('title', 'Cadastro de Empréstimos')
+@section('title', 'Cadastro de Livros')
 
 @section('content')
 <form action="{{ route('books.store') }}" method="POST">
   @csrf
   <legend>Cadastro de Livros</legend>
 
-  <fieldset>
+  <fieldset class="form-fieldset">
     <div class="form-group">
       <div class="linha">
         <div class="campo col-8">
-          <label for="titulo">Título</label>
-          <input type="text" id="titulo" name="titulo">
+          <label for="titulo" class="form-label">Título</label>
+          <input type="text" id="titulo" name="titulo" class="form-control" required>
         </div>
         <div class="campo col-4">
-          <label for="isbn">ISBN</label>
-          <input type="text" id="isbn" name="isbn">
+          <label for="isbn" class="form-label">ISBN</label>
+          <input type="text" id="isbn" name="isbn" class="form-control" required>
         </div>
       </div>
 
       <div class="linha">
         <div class="campo col-12">
-          <label for="autor">Autor</label>
-          <input type="text" id="autor" name="autor">
+          <label for="autor" class="form-label">Autor</label>
+          <input type="text" id="autor" name="autor" class="form-control" required>
         </div>
       </div>
 
       <div class="linha">
         <div class="campo col-12">
-          <label for="categoria">Categoria</label>
-          <input type="text" id="categoria" name="categoria">
+          <label for="categoria" class="form-label">Categoria</label>
+          <input type="text" id="categoria" name="categoria" class="form-control" required>
         </div>
       </div>
     </div>
 
-    <input type="submit" class="mt-5" value="Salvar">
+    <button type="submit" class="btn btn-primary">Salvar</button>
   </fieldset>
 </form>
 @endsection
