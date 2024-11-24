f1ea2a6c286a2eb949b6e82e57b28a6cba57bea6
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 12:22:43 -0300

    feat(controller): adiciona função index para listar alunos

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -7,6 +7,10 @@ use App\Models\Student;
 
 class  StudentController extends Controller
 {
+    public function index() {
+    $students = Student::all(); 
+    return view('students.index', compact('students'));
+  }
 
   public function create()
   {
