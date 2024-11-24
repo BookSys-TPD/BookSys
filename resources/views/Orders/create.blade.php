28c7ef8c2cb9cd239d689753cfa78956f0edbd16
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 14:48:57 -0300

    Refactor: unificação do estilo e estrutura do formulário de cadastro de empréstimos

--- resources/views/Orders/create.blade.php
+++ resources/views/Orders/create.blade.php
@@ -7,34 +7,33 @@
   @csrf
   <legend class="titulo">Cadastro de Empréstimos</legend>
 
-  <fieldset>
+  <fieldset class="form-fieldset">
     <div class="form-group">
       <div class="linha">
         <div class="campo col-6">
-          <label for="matricula">Matrícula</label>
-          <input type="number" id="matricula" name="matricula" required
-            class="{{ session('error_student') ? 'error' : '' }}">
+          <label for="matricula" class="form-label">Matrícula</label>
+          <input type="number" id="matricula" name="matricula" required class="form-control {{ session('error_student') ? 'error' : '' }}">
           @if (session('error_student'))
             <p class="alert">{{ session('error_student') }}</p>
           @endif
         </div>
         <div class="campo col-6">
-          <label for="data_devolucao">Data de Devolução</label>
-          <input type="date" id="data_devolucao" name="data_devolucao" required>
+          <label for="data_devolucao" class="form-label">Data de Devolução</label>
+          <input type="date" id="data_devolucao" name="data_devolucao" required class="form-control">
         </div>
       </div>
 
       <div class="linha">
         <div class="campo col-12">
-          <label for="livro">Livro</label>
-          <input type="text" id="livro" name="livro" required class="{{ session('error_book') ? 'error' : '' }}">
+          <label for="livro" class="form-label">Livro</label>
+          <input type="text" id="livro" name="livro" required class="form-control {{ session('error_book') ? 'error' : '' }}">
           @if (session('error_book'))
             <p class="alert">{{ session('error_book') }}</p>
           @endif
         </div>
       </div>
 
-      <input type="submit" value="Salvar">
+      <button type="submit" class="btn btn-primary">Salvar</button>
     </div>
   </fieldset>
 </form>
