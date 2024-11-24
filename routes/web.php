7ea6558e45fbc6bc96e10f0d8c314bd6af1ec019
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 18:02:39 -0300

    feat(routes): adicionado rota de exclusão para livros

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,7 @@ Route::post('/books', [BookController::class, 'store'])->name('books.store');
 Route::get('/books', [BookController::class, 'index'])->name('books.index');
 Route::get('/books/{id}/edit', [BookController::class, 'edit'])->name('books.edit');
 Route::put('/books/{id}', [BookController::class, 'update'])->name('books.update');
+Route::delete('books/{id}', [BookController::class, 'destroy'])->name('books.destroy');
 
 //Students
 Route::view('/students/create', 'students.create')->name('students.create');
