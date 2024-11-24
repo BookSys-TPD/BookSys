067c41a99277a873ea941da4961c25253992a8e6
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 17:45:47 -0300

    feat(routes): adicionado rota de exclusão para empréstimo

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,4 @@ Route::post('/orders', [OrderController::class, 'store'])->name('orders.store');
 Route::get('/orders', [OrderController::class, 'index'])->name('orders.index');
 Route::get('/orders/{id}/edit', [OrderController::class, 'edit'])->name('orders.edit');
 Route::put('/orders/{id}', [OrderController::class, 'update'])->name('orders.update');
+Route::delete('orders/{id}', [OrderController::class, 'destroy'])->name('orders.destroy');
