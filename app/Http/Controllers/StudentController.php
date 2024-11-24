21bba95a51bb22f5ce51785cfff2437380c4ad62
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 15:51:09 -0300

    feat(controller): adicionar métodos edit e update para edição e atualização de alunos

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -37,4 +37,32 @@ class  StudentController extends Controller
 
     return redirect()->route('students.create')->with('success', 'Estudante cadastrado com sucesso!');
   }
+  public function edit($id)
+  {
+      $student = Student::findOrFail($id);
+      return view('students.edit', compact('student'));
+  }
+
+  public function update(Request $request, $id)
+  {
+      $request->validate([
+          'nome' => 'required|max:255',
+          'endereco' => 'required|max:255',
+          'cpf' => 'required|digits:11|unique:students,cpf,' . $id,
+          'telefone' => 'required|max:15',
+          'email' => 'required|email|max:255|unique:students,email,' . $id,
+      ]);
+
+      $student = Student::findOrFail($id);
+      $student->update([
+          'nome' => $request->nome,
+          'matricula' => $request->matricula,
+          'endereco' => $request->endereco,
+          'cpf' => $request->cpf,
+          'telefone' => $request->telefone,
+          'email' => $request->email,
+      ]);
+
+      return redirect()->route('students.index')->with('success', 'Estudante atualizado com sucesso!');
+  }
 }
