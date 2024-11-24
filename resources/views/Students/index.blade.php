f6a0e8213a5099a02af5596e1ac91914a78cbfcf
Laysa Liima <142900878+LaysaLiima@users.noreply.github.com>
2024-11-24 12:23:39 -0300

    feat(view): criada a view index.blade.php para listagem de alunos

---
+++ resources/views/Students/index.blade.php
@@ -0,0 +1,35 @@
+@extends('layouts.app')
+
+@section('title', 'Alunos')
+
+@section('content')
+<div class="container mt-4">
+  <div class="d-flex justify-content-between align-items-center py-3 border-bottom">
+    <h1 class="titulo">Alunos</h1>
+    <a href="{{ route('students.create') }}" class="btn btn-warning btn-sm">Novo Aluno</a>
+  </div>
+</div>
+
+<div class="container mt-4">
+  <table class="table table-sm table-bordered">
+    <thead class="table-light">
+      <tr>
+        <th>Código</th>
+        <th>Nome</th>
+        <th>Telefone</th>
+        <!-- <th>Editar</th>
+        <th>Desativar</th> -->
+      </tr>
+    </thead>
+    <tbody>
+      @foreach ($students as $student)
+      <tr>
+      <td>{{ $student->id }}</td>
+      <td>{{ $student->nome }}</td>
+      <td>{{ $student->telefone }}</td>
+      </tr>
+    @endforeach
+    </tbody>
+  </table>
+</div>
+@endsection
